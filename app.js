d81e7e3b5232c802e2d6ca29a7684a027030ae5c
Jen Liu <jennifer@open.gov.sg>
2023-08-09 11:03:50 -0400

    Updating app.listen to include env PORT

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ postsArray.forEach(function(post) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
 
