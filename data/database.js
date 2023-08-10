278722be451b167aa30a1bdf50ea0863122d82ef
Jen Liu <jennifer@open.gov.sg>
2023-08-10 09:12:44 -0400

    Troubleshooting attempt #31

--- data/database.js
+++ data/database.js
@@ -1,10 +1,12 @@
+console.log("Hello World");
+
 // require("dotenv").config();
 
 //We're using the Postgres package
-const { Client } = require("pg");
+// const { Client } = require("pg");
 
 //we're using different packages to enable the App to find the right path for obtaining the SSL Certs
-// const fs = require("fs");
+// const fs = reequire("fs");
 // const path = require("path");
 // const caCertPath = path.join(__dirname,"..","certs","global-bundle.pem");
 // const caCert = fs.readFileSync(caCertPath);
